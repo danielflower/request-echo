d19f74cfe6462c3952ceaf16405094883f246b31
Daniel Flower <git@danielflower.com>
2018-09-02 19:50:35 +0800

    An app that just echos the request path and headers

--- src/main/java/requestecho/App.java
+++ src/main/java/requestecho/App.java
@@ -1,65 +1,42 @@
 package requestecho;
 
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.util.resource.Resource;
+import io.muserver.MuServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.InetSocketAddress;
+import java.io.PrintWriter;
 import java.util.Map;
 
+import static io.muserver.MuServerBuilder.muServer;
+
 public class App {
-    public static final Logger log = LoggerFactory.getLogger(App.class);
-    public static void main(String[] args) throws Exception {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
         Map<String, String> settings = System.getenv();
-
-        // When run from app-runner, you must use the port set in the environment variable APP_PORT
         int port = Integer.parseInt(settings.getOrDefault("APP_PORT", "7928"));
-        // All URLs must be prefixed with the app name, which is got via the APP_NAME env var.
         String appName = settings.getOrDefault("APP_NAME", "request-echo");
-        String env = settings.getOrDefault("APP_ENV", "local"); // "prod" or "local"
-        boolean isLocal = "local".equals(env);
-        log.info("Starting " + appName + " in " + env + " on port " + port);
 
-        Server jettyServer = new Server(new InetSocketAddress("localhost", port));
-        jettyServer.setStopAtShutdown(true);
+        MuServer server = muServer()
+            .withHttpPort(port)
+            .addHandler((req, resp) -> {
+                resp.contentType("text/plain");
 
-        HandlerList handlers = new HandlerList();
-        // TODO: set your own handlers
-        handlers.addHandler(resourceHandler(isLocal));
+                PrintWriter writer = resp.writer();
+                writer.println("Request URI: " + req.uri());
+                writer.println("Server URI: " + req.serverURI());
+                writer.println("Server info: " + req.server());
+                writer.println("Headers:");
+                for (Map.Entry<String, String> entry : req.headers().entries()) {
+                    writer.println("    " + entry.getKey() + ": " + entry.getValue());
+                }
+                writer.println("End of message");
 
-        // you must serve everything from a directory named after your app
-        ContextHandler ch = new ContextHandler();
-        ch.setContextPath("/" + appName);
-        ch.setHandler(handlers);
-        jettyServer.setHandler(ch);
+                return false;
+            })
+            .start();
 
-        try {
-            jettyServer.start();
-        } catch (Throwable e) {
-            log.error("Error on start", e);
-            System.exit(1);
-        }
-
-        log.info("Started app at http://localhost:" + port + ch.getContextPath());
-        jettyServer.join();
+        log.info("Started app at " + server.uri().resolve("/" + appName));
     }
 
-    private static Handler resourceHandler(boolean useFileSystem) {
-        ResourceHandler resourceHandler = new ResourceHandler();
-        if (useFileSystem) {
-            resourceHandler.setResourceBase("src/main/resources/web");
-            resourceHandler.setMinMemoryMappedContentLength(-1);
-        } else {
-            resourceHandler.setBaseResource(Resource.newClassPathResource("/web", true, false));
-        }
-        resourceHandler.setEtags(true);
-        resourceHandler.setWelcomeFiles(new String[] {"index.html"});
-        return resourceHandler;
-    }
 
 }
